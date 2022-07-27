2c90c9c31c2c60bd13d45e3a6fea906b4fdae6c6
Carlos Villar <carlosvrob91@gmail.com>
2022-07-27 15:51:12 +0200

    Test CalculatorApplication + Modificación pom

--- src/test/java/com/proyectoscvr/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/proyectoscvr/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,22 @@
 package com.proyectoscvr.calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CalculatorApplicationTests {
-
+	
+	@Autowired
+    ApplicationContext context;
+	
 	@Test
 	void contextLoads() {
+		CalculatorApplication.main(new String[] {"test1", "test2"});
+        assertThat(context).isNotNull();
 	}
 
 }
